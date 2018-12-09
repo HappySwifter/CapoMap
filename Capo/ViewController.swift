374e0443fdcc745bf830ae8f234d16a5777ec218
Artem <artem.valiev@gmail.com>
2018-12-09 19:52:38 +0300

    Initial Commit

---
+++ Capo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capo
+//
+//  Created by Артем Валиев on 09/12/2018.
+//  Copyright © 2018 Артем Валиев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
